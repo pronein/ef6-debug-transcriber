475a1a91e43ab74636d5cfa565f80931952aef92
Adam Schrader <adam.dot.schrader@gmail.com>
2018-03-17 12:53:00 -0400

    Add project files.

---
+++ EF6DebugTranscriber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6DebugTranscriber {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
